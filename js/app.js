1a761ef9bc566d6207ce52cd27a0d52219984814
Antonio Funicella <amf9189@mavs.uta.edu>
2022-05-01 12:51:27 -0500

    Nav Bar and Footer Added
    The navbar and footer should be function, and on every page

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(function(){
+// Navigation Interations 
+//     1- Menu Toggler
+//     2-Dropdown Toggler
+
+// Menu Toggler
+// 1. listen for a click on .navbar-toggler
+// 2. store the property in the attribute "datatarget"
+//3. use the attribute's value to select the navigation it wants to affect
+$(".navbar-toggler").on("click", function(){ 
+    let toggler = $(this).attr("data-target");
+   $(toggler).toggleClass("open");
+})
+
+
+
+})
